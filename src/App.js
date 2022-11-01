abd473d2de4165311aa149dbd7a720de05e31265
Mark Carlson <mcarlson@avenuecode.com>
2022-10-31 20:56:03 -0700

    Updates home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Everyone.  Welcome to my react app!
         </p>
         <a
           className="App-link"
